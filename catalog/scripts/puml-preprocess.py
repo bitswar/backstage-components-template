f4e7557bc635347efb10938966942d47f2fc5223
danysmall <dany.small.work@gmail.com>
2023-06-23 18:44:13 +0300

    Updated preprocessor and makefile

---
+++ catalog/scripts/puml-preprocess.py
@@ -0,0 +1,131 @@
+import os
+import pickle
+import sys
+
+src_dir = 'docs'
+utils_dir = 'docs/diagrams/utils'
+extensions = ['.puml']
+
+
+class Processer:
+    dump_file_name = 'dump.pkl'
+
+    def preprocess(self):
+        diagrams = self.__get_all_diagram_files(src_dir)
+        utils = {}
+        for folder in Env().include_folders:
+            utils.update(self.__get_all_diagram_files(folder))
+
+        for u in utils.keys():
+            for i in diagrams.keys():
+                if i == u:
+                    diagrams.pop(i)
+                    break
+
+        for path in diagrams.values():
+            self.__dump_sources(path)
+            self.__preprocess_file(path, utils)
+
+    def __get_all_diagram_files(self, start_path: str) -> dict[str, str]:
+        result = {}
+        for item in os.walk(start_path):
+            for file in item[2]:
+                for ext in extensions:
+                    if not file.find(ext) == -1:
+                        result[file] = f'{item[0]}/{file}'
+        return result
+
+    def __preprocess_file(self, filepath: str, utils: dict[str, str]) -> None:
+        content = ''
+        with open(filepath, 'r') as file:
+            content = file.read().split('\n')
+
+        for line in range(len(content)):
+            for key in utils:
+                if not content[line].find(key) == -1:
+                    with open(utils[key], 'r') as template:
+                        content[line] = '\n'.join(
+                            template.read().split('\n')[1:-1]
+                        )
+
+        with open(filepath, 'w') as file:
+            file.write('\n'.join(content))
+
+    def __dump_sources(self, path: str) -> None:
+        with open(path, 'r') as file:
+            data = FileData(
+                path,
+                path.split('/')[-1],
+                file.read()
+            )
+
+        with open(self.dump_file_name, 'ab+') as file:
+            pickle.dump(data, file, pickle.HIGHEST_PROTOCOL)
+
+    def __load_dumped(self) -> list['FileData']:
+        loaded = []
+        with open(self.dump_file_name, 'rb') as file:
+            try:
+                object = pickle.load(file)
+                loaded.append(object)
+            except Exception:
+                pass
+        return loaded
+
+    def restore(self) -> None:
+        files = self.__load_dumped()
+        for file in files:
+            with open(file.path, 'w') as dest:
+                dest.write(file.content)
+
+
+class FileData:
+    content: str
+    path: str
+    filename: str
+
+    def __init__(self, path: str, filename: str, content: str) -> None:
+        self.content = content
+        self.filename = filename
+        self.path = path
+
+
+class Env:
+    include_folders: list[str]
+
+    env_folder_pattern = 'PUML_INCLUDE_DIR'
+
+    def __init__(self) -> None:
+        self.include_folders = self.__load_envs()
+
+    def __load_envs(self) -> list[str]:
+        envs = []
+        with open('.env', 'r') as file:
+            envs = file.read().split('\n')
+
+        folders = []
+        for line in envs:
+            if not line.find(self.env_folder_pattern) == -1:
+                path = line.split('=')[-1]
+                if path[-1] == '/':
+                    path = path[:-1]
+                if path[0] == '/':
+                    path = path[1:]
+                folders.append(path)
+        return folders
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == 'process':
+        processer = Processer()
+        processer.preprocess()
+        print('Process')
+    elif len(sys.argv) > 1 and sys.argv[1] == 'restore':
+        print('Restore')
+        processer = Processer()
+        processer.restore()
+    else:
+        print([
+            'Not passes action argument.',
+            f'Common usage: python {sys.argv[0]} [process|restore]',
+        ], sep='\n')
